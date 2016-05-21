1da0012602725937e8ba0059c03ee2ae23f61b51
447964249 <laiyawen@qq.com>
2016-05-21 14:27:07 +0800

    改包名！

---
+++ src/cn/ucai/superkache/domain/VideoEntity.java
@@ -0,0 +1,9 @@
+package cn.ucai.superkache.domain;
+
+public class VideoEntity {
+	public int ID;
+	public String title;
+	public String filePath;
+	public int size;
+	public int duration;
+}
---
+++ src/cn/ucai/superkache/utils/UserUtils.java
@@ -0,0 +1,91 @@
+package cn.ucai.superkache.utils;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import cn.ucai.superkache.applib.controller.HXSDKHelper;
+import cn.ucai.superkache.DemoHXSDKHelper;
+import cn.ucai.superkache.domain.EMUser;
+import com.squareup.picasso.Picasso;
+
+public class UserUtils {
+    /**
+     * 根据username获取相应user，由于demo没有真实的用户数据，这里给的模拟的数据；
+     * @param username
+     * @return
+     */
+    public static EMUser getUserInfo(String username){
+        EMUser user = ((DemoHXSDKHelper) HXSDKHelper.getInstance()).getContactList().get(username);
+        if(user == null){
+            user = new EMUser(username);
+        }
+            
+        if(user != null){
+            //demo没有这些数据，临时填充
+        	if(TextUtils.isEmpty(user.getNick()))
+        		user.setNick(username);
+        }
+        return user;
+    }
+    
+    /**
+     * 设置用户头像
+     * @param username
+     */
+    public static void setUserAvatar(Context context, String username, ImageView imageView){
+    	EMUser user = getUserInfo(username);
+        if(user != null && user.getAvatar() != null){
+            Picasso.with(context).load(user.getAvatar()).placeholder(cn.ucai.superkache.R.drawable.default_avatar).into(imageView);
+        }else{
+            Picasso.with(context).load(cn.ucai.superkache.R.drawable.default_avatar).into(imageView);
+        }
+    }
+    
+    /**
+     * 设置当前用户头像
+     */
+	public static void setCurrentUserAvatar(Context context, ImageView imageView) {
+		EMUser user = ((DemoHXSDKHelper)HXSDKHelper.getInstance()).getUserProfileManager().getCurrentUserInfo();
+		if (user != null && user.getAvatar() != null) {
+			Picasso.with(context).load(user.getAvatar()).placeholder(cn.ucai.superkache.R.drawable.default_avatar).into(imageView);
+		} else {
+			Picasso.with(context).load(cn.ucai.superkache.R.drawable.default_avatar).into(imageView);
+		}
+	}
+    
+    /**
+     * 设置用户昵称
+     */
+    public static void setUserNick(String username,TextView textView){
+    	EMUser user = getUserInfo(username);
+    	if(user != null){
+    		textView.setText(user.getNick());
+    	}else{
+    		textView.setText(username);
+    	}
+    }
+    
+    /**
+     * 设置当前用户昵称
+     */
+    public static void setCurrentUserNick(TextView textView){
+    	EMUser user = ((DemoHXSDKHelper)HXSDKHelper.getInstance()).getUserProfileManager().getCurrentUserInfo();
+    	if(textView != null){
+    		textView.setText(user.getNick());
+    	}
+    }
+    
+    /**
+     * 保存或更新某个用户
+     * @param user
+     */
+	public static void saveUserInfo(EMUser newUser) {
+		if (newUser == null || newUser.getUsername() == null) {
+			return;
+		}
+		((DemoHXSDKHelper) HXSDKHelper.getInstance()).saveContact(newUser);
+	}
+    
+}
