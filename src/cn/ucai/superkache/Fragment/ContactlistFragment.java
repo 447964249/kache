efda9eec0b204e350709ba7121b48178b16eee33
447964249 <laiyawen@qq.com>
2016-05-24 21:25:59 +0800

    adapter修改V2

--- src/cn/ucai/superkache/Fragment/ContactlistFragment.java
+++ src/cn/ucai/superkache/Fragment/ContactlistFragment.java
@@ -85,7 +85,7 @@ import com.easemob.util.EMLog;
 public class ContactlistFragment extends Fragment {
     public static final String TAG = "ContactlistFragment";
     private ContactAdapter adapter;
-    private List<EMUser> contactList;
+    private List<Contact> contactList;
     private ListView listView;
     private boolean hidden;
     private Sidebar sidebar;
@@ -98,7 +98,7 @@ public class ContactlistFragment extends Fragment {
     HXContactInfoSyncListener contactInfoSyncListener;
     View progressBar;
     Handler handler = new Handler();
-    private EMUser toBeProcessUser;
+    private Contact toBeProcessUser;
     private String toBeProcessUsername;
 
     ContactListChangedReceiver mReceiver;
@@ -186,7 +186,7 @@ public class ContactlistFragment extends Fragment {
 
         //黑名单列表
         blackList = EMContactManager.getInstance().getBlackListUsernames();
-        contactList = new ArrayList<EMUser>();
+        contactList = new ArrayList<Contact>();
         mContactList = new ArrayList<Contact>();
         // 获取设置contactlist
         getContactList();
@@ -221,13 +221,13 @@ public class ContactlistFragment extends Fragment {
         });
 
         // 设置adapter
-        adapter = new ContactAdapter(getActivity(), R.layout.row_contact, contactList);
+        adapter = new ContactAdapter(getActivity(),R.layout.row_contact, mContactList);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new OnItemClickListener() {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String username = adapter.getItem(position).getUsername();
+                String username = adapter.getItem(position).getMContactCname();
                 if (Constant.NEW_FRIENDS_USERNAME.equals(username)) {
                     // 进入申请与通知页面
                     EMUser user = ((DemoHXSDKHelper) HXSDKHelper.getInstance()).getContactList().get(Constant.NEW_FRIENDS_USERNAME);
@@ -244,7 +244,7 @@ public class ContactlistFragment extends Fragment {
                     startActivity(new Intent(getActivity(), RobotsActivity.class));
                 } else {
                     // demo中直接进入聊天页面，实际一般是进入用户详情页
-                    startActivity(new Intent(getActivity(), ChatActivity.class).putExtra("userId", adapter.getItem(position).getUsername()));
+                    startActivity(new Intent(getActivity(), ChatActivity.class).putExtra("userId", adapter.getItem(position).getMContactCname()));
                 }
             }
         });
@@ -296,7 +296,7 @@ public class ContactlistFragment extends Fragment {
         super.onCreateContextMenu(menu, v, menuInfo);
         if (((AdapterContextMenuInfo) menuInfo).position > 1) {
             toBeProcessUser = adapter.getItem(((AdapterContextMenuInfo) menuInfo).position);
-            toBeProcessUsername = toBeProcessUser.getUsername();
+            toBeProcessUsername = toBeProcessUser.getMContactCname();
             getActivity().getMenuInflater().inflate(R.menu.context_contact_list, menu);
         }
     }
@@ -309,7 +309,7 @@ public class ContactlistFragment extends Fragment {
                 deleteContact(toBeProcessUser);
                 // 删除相关的邀请消息
                 InviteMessgeDao dao = new InviteMessgeDao(getActivity());
-                dao.deleteMessage(toBeProcessUser.getUsername());
+                dao.deleteMessage(toBeProcessUser.getMContactCname());
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -343,7 +343,7 @@ public class ContactlistFragment extends Fragment {
      *
      * @param
      */
-    public void deleteContact(final EMUser tobeDeleteUser) {
+    public void deleteContact(final Contact tobeDeleteUser) {
         String st1 = getResources().getString(R.string.deleting);
         final String st2 = getResources().getString(R.string.Delete_failed);
         final ProgressDialog pd = new ProgressDialog(getActivity());
@@ -353,11 +353,11 @@ public class ContactlistFragment extends Fragment {
         new Thread(new Runnable() {
             public void run() {
                 try {
-                    EMContactManager.getInstance().deleteContact(tobeDeleteUser.getUsername());
+                    EMContactManager.getInstance().deleteContact(tobeDeleteUser.getMContactCname());
                     // 删除db和内存中此用户的数据
                     EMUserDao dao = new EMUserDao(getActivity());
-                    dao.deleteContact(tobeDeleteUser.getUsername());
-                    ((DemoHXSDKHelper) HXSDKHelper.getInstance()).getContactList().remove(tobeDeleteUser.getUsername());
+                    dao.deleteContact(tobeDeleteUser.getMContactCname());
+                    ((DemoHXSDKHelper) HXSDKHelper.getInstance()).getContactList().remove(tobeDeleteUser.getMContactCname());
                     getActivity().runOnUiThread(new Runnable() {
                         public void run() {
                             pd.dismiss();
